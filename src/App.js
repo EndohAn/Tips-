0718bd721f3e2657951805d6b2ba47153c8f4b2e
unknown <flowerrosa76@gmail.com>
2023-01-28 14:19:19 +0900

    v.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useEffect,useState} from 'react';
+
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const[advice,setAdvice] = useState('');
+  const[newAdvice,setNewAdvice]=useState(advice);
+
+  
+  useEffect(()=>{
+    const getApi =async()=>{
+      const response =await fetch('http://www.boredapi.com/api/activity/');
+      const data =await response.json();
+      console.log(data.activity);
+      setAdvice(data.activity)
+    }
+    getApi();
+    
+    
+  },[newAdvice])
+
+  const changeAdvice =()=>{
+   setAdvice(setNewAdvice)
+  }
+
+
+ return(
+  <div className='App'>
+    <div className='container'>
+      <h2>Tips for every day</h2>
     </div>
-  );
+    <div className='container'>
+      <p>{advice}</p>
+    </div>
+    <div className='container'>
+      <button onClick ={changeAdvice}>Change your advace</button>
+    </div>
+  </div>
+ )
+
+
 }
 
 export default App;
